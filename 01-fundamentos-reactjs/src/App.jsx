9aca39687173c6644374aa794c653c42f5fbcd98
Fernanda Degolin <fernanda.degolin@origoenergia.com.br>
2025-04-20 00:49:34 -0300

    feat: implement Header and Post components with styling

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,19 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+import "./styles.css";
+
 export function App() {
-  return <h1>Fernanda</h1>;
+  return (
+    <>
+      <Header />
+      <Post
+        author="Fernanda Degolin"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate doloribus dolor reprehenderit mollitia illo laboriosam porro autem placeat velit odit sint nisi omnis laudantium illum nobis quia doloremque, sed consequatur."
+      />
+      <Post
+        author="Fernanda Degolin"
+        content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate doloribus dolor reprehenderit mollitia illo laboriosam porro autem placeat velit odit sint nisi omnis laudantium illum nobis quia doloremque, sed consequatur."
+      />
+    </>
+  );
 }
-
-
